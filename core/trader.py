0ed59687420f3cf66b814b7344d67ea55503ecba
Bogyu Park <bogyupark@Bogyuui-MacBookAir.local>
2026-01-24 08:04:13 +0900

    telegram bot ver downgrade

--- core/trader.py
+++ core/trader.py
@@ -12,11 +12,11 @@ from datetime import datetime
 import pandas as pd
 import traceback
 import telegram
-# from telegram.ext import *
-from telegram import Bot, Update
-from telegram.ext import (
-    ApplicationBuilder, MessageHandler, ContextTypes, filters
-)
+from telegram.ext import *
+# from telegram import Bot, Update
+# from telegram.ext import (
+#     ApplicationBuilder, MessageHandler, ContextTypes, filters
+# )
 import asyncio
 from core.user import UserData
 
